5f877d821b2208cb167d10f930a28a6e9f79b5fc
Stephen Spanos <sspanos@gmail.com>
2019-05-07 20:24:51 -0400

    My Django Girls app, first commit

---
+++ myDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/stephenspanos/PycharmProjects/djangoGirls/myDjango/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ testing.py
@@ -0,0 +1,7 @@
+def hi(name):
+    print("Hi " + name + "!")
+
+boys = ['Bob','Bill','John','Joe']
+
+for a in boys:
+    hi(a)
